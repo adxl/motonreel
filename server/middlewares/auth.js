e683ec8322859943a534de9b5a9281ea7930bda9
Adel Sen <adelsenhadjii@gmail.com>
2022-10-28 13:04:03 +0200

    Feat: add jwt middleware

---
+++ server/middlewares/auth.js
@@ -0,0 +1,31 @@
+const { verifyToken } = require('../lib/jwt');
+const { User } = require('../models');
+
+module.exports = async (req, res, next) => {
+  const header = req.headers.authorization;
+  if (!header) {
+    return res.sendStatus(401);
+  }
+
+  const [type, token] = header.split(/\s+/);
+  if (type !== 'Bearer') {
+    return res.sendStatus(401);
+  }
+
+  const user = await verifyToken(token);
+  if (user) {
+    req.user = await User.findByPk(
+      user.email,
+      {
+        // permet de récupérer des données de jointure
+        // include: [],
+      },
+    );
+
+    if (!req.user) return res.sendStatus(404);
+
+    return next();
+  }
+
+  return res.sendStatus(401);
+};
